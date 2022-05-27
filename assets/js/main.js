aa55a0b76b09e4e9d76212384385b0055d82de32
orgoloth <orgoloth@gmail.com>
2022-05-27 20:25:42 +0200

    prevent submits

---
+++ assets/js/main.js
@@ -0,0 +1,2 @@
+document.querySelector('#search-submit').addEventListener('click', (event) => event.preventDefault());
+document.querySelector('#newsletter-submit').addEventListener('click', (event) => event.preventDefault());
